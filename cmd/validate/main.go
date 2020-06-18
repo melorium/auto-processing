d95e865a1abadc09295b119b5baa6cf51fc0771e
simonjanss <simon.jansson1@hotmail.com>
2020-06-18 14:02:11 +0200

    Added config-validator

---
+++ cmd/validate/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/avian-digital-forensics/auto-processing/config"
+	"github.com/google/martian/log"
+)
+
+func main() {
+	flags := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	cfgPath := flags.String("cfg", "/configs/config.yml", "filepath for the config")
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		fmt.Printf("Failed to get cfg, use flag: --cfg=/path/to/cfg - error: %v", err)
+		os.Exit(2)
+	}
+
+	cfg, err := config.GetConfig(*cfgPath)
+	if err != nil {
+		fmt.Printf("Failed to get config: %s - %v", *cfgPath, err)
+		os.Exit(2)
+	}
+
+	licenseCfg, err := config.GetConfig("license.yml")
+	if err != nil {
+		log.Error("Failed to get config for license: %s - %v", *cfgPath, err)
+		os.Exit(2)
+	}
+
+	cfg.Server = licenseCfg.Server
+
+	if err := cfg.Validate(); err != nil {
+		fmt.Printf("Error when validating config: %s - %v", *cfgPath, err)
+		os.Exit(2)
+	}
+	fmt.Printf("Config OK : %s", *cfgPath)
+}
