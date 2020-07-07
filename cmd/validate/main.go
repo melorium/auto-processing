5d2ae4cdadcbb08f4eea09289453f9ed03ed9f9c
simonjanss <simon.jansson1@hotmail.com>
2020-07-07 05:47:40 +0200

    validate multiple configs

--- cmd/validate/main.go
+++ cmd/validate/main.go
@@ -3,36 +3,54 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 
 	"github.com/avian-digital-forensics/auto-processing/config"
 )
 
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return "my string representation"
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+var configs arrayFlags
+
 func main() {
-	flags := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	cfgPath := flags.String("cfg", "./configs/auto-processing.yml", "filepath for the config")
-	if err := flags.Parse(os.Args[1:]); err != nil {
-		fmt.Printf("Failed to get cfg, use flag: --cfg=/path/to/cfg - error: %v", err)
-		os.Exit(2)
+	flag.Var(&configs, "cfg", "Path for the config to validate")
+	flag.Parse()
+
+	var result []string
+
+	for _, cfgPath := range configs {
+		cfg, err := config.GetConfig(cfgPath)
+		if err != nil {
+			msg := fmt.Sprintf("Validate FAILED: %s - Failed to get config: %v", cfgPath, err)
+			result = append(result, msg)
+			continue
+		}
+
+		licenseCfg, err := config.GetConfig("./configs/license.yml")
+		if err != nil {
+			msg := fmt.Sprintf("Validate FAILED: %s - Failed to get license-config: %v", cfgPath, err)
+			result = append(result, msg)
+			continue
+		}
+
+		cfg.Server = licenseCfg.Server
+
+		if err := cfg.Validate(); err != nil {
+			msg := fmt.Sprintf("Validate FAILED: %s - %v", cfgPath, err)
+			result = append(result, msg)
+			continue
+		}
 	}
 
-	cfg, err := config.GetConfig(*cfgPath)
-	if err != nil {
-		fmt.Printf("Failed to get config: %s - %v", *cfgPath, err)
-		os.Exit(2)
+	for _, msg := range result {
+		fmt.Println(msg)
 	}
-
-	licenseCfg, err := config.GetConfig("./configs/license.yml")
-	if err != nil {
-		fmt.Printf("Failed to get config for license: %s - %v", *cfgPath, err)
-		os.Exit(2)
-	}
-
-	cfg.Server = licenseCfg.Server
-
-	if err := cfg.Validate(); err != nil {
-		fmt.Printf("Error when validating config: %s - %v", *cfgPath, err)
-		os.Exit(2)
-	}
-	fmt.Printf("Config OK : %s", *cfgPath)
 }
